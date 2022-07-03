2b9c059e7c04d546652a4e1e173354e4d95e3dc0
Haroon Asif <haroon.asif@tkxel.com>
2022-07-03 20:12:31 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/myapp/flutter_projects/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.myapp.flutter_projects
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:Home()
+
+
+  ));
+}
+
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  return (
+      Scaffold(
+        appBar: AppBar(
+          title: Text('My first App'),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+
+        ),
+
+        body: Center(
+        
+            // child:Image(image: AssetImage("assets/splash.jpg"),
+        
+          // child: Image.asset('assets/splash.jpg'),
+        
+        child: RaisedButton(
+          onPressed: (){},
+          child: Text('Click Me'),
+          color: Colors.amber,
+        ),
+        
+        // Text("Hello",
+        //   style: TextStyle(
+        //     fontSize: 20,
+        //     fontWeight: FontWeight.bold,
+        //     color: Colors.black,
+        //
+        //   ),
+
+
+        ),
+
+
+        floatingActionButton: FloatingActionButton(onPressed: () => {},
+          child: Text("Press"),
+          backgroundColor: Colors.amber,
+
+        ),
+        )
+      
+
+
+
+  );
+  }
+}
+
